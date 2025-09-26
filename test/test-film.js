b6c031991bca4e7ffce72848be91d1f568762b50
INT161 <INT161@Train10-4-PC090.control.ad.sit.kmutt.ac.th>
2025-09-26 14:37:10 +0700

    add a filter

--- test/test-film.js
+++ test/test-film.js
@@ -1,9 +1,14 @@
 const {PrismaClient} = require('../generated/prisma');
 const prisma = new PrismaClient();
 
-async function getAllFilms() {
-    const films = await prisma.Film.findMany();
+async function getAllFilms(filterTitle) {
+    const films = await prisma.Film.findMany(
+        {
+            where : { title : {contains : filterTitle}},
+            orderBy : {id : 'desc'}
+        }
+    );
     console.log(films);
 }
 
-getAllFilms()
+getAllFilms("love")
